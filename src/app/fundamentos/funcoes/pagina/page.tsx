33005e0524fda6998aaf50b6aeb49732e19f638f
Rafael Iagher Costa <fael.iagher28082007@gmail.com>
2025-07-11 05:18:54 -0700

    componentes com props

--- src/app/fundamentos/funcoes/pagina/page.tsx
+++ src/app/fundamentos/funcoes/pagina/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     return(
         <>
         <div className={`flex-col gap-4 p-4 h-screen`}>Montar uma pagina</div>
-        <Cabecalho />
+        <Cabecalho  titulo="Minha aplicacao" subtitulo="melhor aplicacao web"/>
         <Conteudo />
         <Rodape />
         </>
--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,7 +1,8 @@
-export default function Cabecalho () {
+export default function Cabecalho (props: any) {
     return(
-        <div className="  flex justify-center items-center h-36 bg-purple-500 text-3xl rounded-lg">
-            Cabecalho
+        <div className="  flex flex-col justify-center items-center h-36 bg-purple-500 rounded-lg">
+            <h1 className="text-3xl font-black">{props.titulo}</h1>
+            <h2>{props.subtitulo}</h2>
         </div>
     )
     
