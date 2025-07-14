9c631e63ad914e3903444f6b7a28875741f74978
Rafael Iagher Costa <fael.iagher28082007@gmail.com>
2025-07-14 03:50:54 -0700

    rodape

--- src/app/fundamentos/funcoes/pagina/page.tsx
+++ src/app/fundamentos/funcoes/pagina/page.tsx
@@ -6,10 +6,11 @@ import Rodape from "@/components/Rodape"
 export default function Page() {
     return(
         <>
-        <div className={`flex-col gap-4 p-4 h-screen`}>Montar uma pagina</div>
+        <div className={`flex-col gap-4 p-4 h-screen`}>
         <Cabecalho  titulo="Minha aplicacao" subtitulo="melhor aplicacao web"/>
-        <Conteudo />
-        <Rodape />
+        <Conteudo/>
+        <Rodape  feito ="Feito com amor por Rafael Iagher" copy ="CopyRightdwadwadwadwadwadwa"/>
+        </div>
         </>
     )
 }
--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,6 +1,10 @@
-export default function Rodape() {
+export default function Rodape(props: any) {
 
     return(
-        <div className="flex justify-center items-center h-36 rounded-lg text-3xl bg-cyan-600">Rodape</div>
+        <div className="flex justify-between items-center h-36 rounded-lg text-2xl px-10 bg-cyan-600">
+
+            <h3>{props.feito}</h3>
+            <h3>{props.copy}</h3>
+        </div>
     )
 }
